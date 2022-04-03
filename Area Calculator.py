4e84f88ecc39840c1b8a7c7c417df69738f82ac9
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-02 19:58:20 -0500

    area calculator

---
+++ Area Calculator.py
@@ -0,0 +1,8 @@
+#This program calculates the area of a rectangle
+
+side_a = int(input("What is the measurement of the length: "))
+side_b = int(input("What is the measurement of the width: "))
+
+area = side_a * side_b
+
+print("The area of the rectangle is", area)
