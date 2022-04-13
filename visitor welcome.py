e6247a43eb88ede85606aca1f0ad299171ac98fb
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-04-12 19:13:19 -0500

    code for my "Welcome" greeting

---
+++ visitor welcome.py
@@ -0,0 +1,4 @@
+first_name = str(input("Hello, what is your first name? "))
+last_name = str(input("Please tell us your last name. "))
+
+print("Nice to meet you" + " " + first_name + " " + last_name + " " + "!")
